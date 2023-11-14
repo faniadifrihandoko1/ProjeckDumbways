0115970316851c02ba9b17938b5721504283503b
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2023-11-15 00:15:57 +0700

    add feature: Page testimonial and OOP

--- Day7/assets/js/testimonial.js
+++ Day7/assets/js/testimonial.js
@@ -18,17 +18,17 @@ class Testimonials {
 
 const testi1 = new Testimonials(
   "https://media.npr.org/assets/img/2022/11/08/ap22312071681283-0d9c328f69a7c7f15320e8750d6ea447532dff66-s1100-c50.jpg",
-  "Bagus dan tahan lama, mantap",
+  "Websitenya bagus, sudah responsive",
   "fani"
 );
 const testi2 = new Testimonials(
   "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=ais",
-  "Bagus dan tahan lama, mantap",
+  "Bagus websitenya, pengerjaanya cepat",
   "Febri"
 );
 const testi3 = new Testimonials(
   "https://img.freepik.com/premium-photo/portrait-smiling-young-man-looking-camera_33839-1731.jpg",
-  "Bagus dan tahan lama, mantap",
+  "Tampilan websitenya menarik dan bagus",
   "Bayu"
 );
 
