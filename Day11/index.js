ea554953b83c0119e4908400b50884a053506da4
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2023-11-22 00:58:55 +0700

    add Feature:Method GET and POST

---
+++ Day11/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 5000;
+
+// app.set = buat setting variabel global, configuration dll
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/views"));
+
+// menampung midlleware
+app.use("/assets", express.static("src/assets"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/home", home);
+app.get("/project", project);
+app.post("/project", addProject);
+app.get("/project/:id", detailProject);
+app.get("/contact-me", contactMe);
+app.get("/testimonials", testimonials);
+
+function home(req, res) {
+  res.render("index");
+}
+
+function project(req, res) {
+  res.render("add-project");
+}
+
+function addProject(req, res) {
+  const name = req.body.inputTitle;
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
+  const message = req.body.inputMessage;
+  const tech = req.body.tech;
+  const image = req.body.inputImage;
+  console.log(`name : ${name}`);
+  console.log(`Start Project: ${startDate}`);
+  console.log(`Finish Project: ${endDate}`);
+  console.log(`Technologies: ${tech}`);
+  console.log(`Message: ${message}`);
+  console.log(`Image: ${image}`);
+
+  res.redirect("home");
+}
+
+function detailProject(req, res) {
+  const title = req.params.id;
+  const data = {
+    id: "1",
+    name: `Project`,
+  };
+  res.render("detail-project");
+}
+function contactMe(req, res) {
+  res.render("contact");
+}
+
+function testimonials(req, res) {
+  res.render("testimonials");
+}
+
+app.listen(port, () => {
+  console.log(`Server Berjalan di port${port}`);
+});
---
+++ Day11/src/assets/js/contact.js
@@ -0,0 +1,43 @@
+// let Fullname = "fani adi frihandoko";
+// let Address = "Kp. Belimbing Sawah";
+
+// function Hello(name, alamat) {
+//   console.log(`Perkenalkan nama saya ${name}, Alamat Saya di ${alamat}`);
+// }
+
+// Hello(Fullname, Address);
+
+const articel1 = {
+  judul: "Bola",
+  Deskripsi: "",
+  author: "",
+  kategori: ""
+  
+
+}
+
+function submitData() {
+  const inputName = document.getElementById("inputName").value;
+  const inputEmail = document.getElementById("inputEmail").value;
+  const inputPhone = document.getElementById("inputPhone").value;
+  const inputSubject = document.getElementById("inputSubject").value;
+  const inputMessage = document.getElementById("inputMessage").value;
+  // console.log(inputSubject);
+
+  // -- Validasi -- //
+  if (inputName == 0) {
+    alert("please fill in Name");
+  } else if (inputEmail == 0) {
+    alert("please fill in Email");
+  } else if (inputPhone == 0) {
+    alert("please fill in Number Phone");
+  } else if (inputSubject == 0) {
+    alert("please choose a Subject");
+  } else if (inputMessage == 0) {
+    alert("please fill in Message");
+  } else {
+    let a = document.createElement("a");
+    a.href = `mailto:${inputEmail}?subject=${inputSubject}&body=${inputMessage}`;
+    a.click();
+  }
+}
